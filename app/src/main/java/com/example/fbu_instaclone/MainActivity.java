3a0171e242cb83dd0ab5f4cc479e80c61050b7d0
Anthony <anthony.pena0898@gmail.com>
2021-06-20 17:59:39 -0400

    UI changes

--- app/src/main/java/com/example/fbu_instaclone/MainActivity.java
+++ app/src/main/java/com/example/fbu_instaclone/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         context = this;
 
