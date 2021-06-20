53d54df5446dd2325eac1dacd4b8936fdbf0ff56
Anthony <anthony.pena0898@gmail.com>
2021-06-19 20:41:36 -0400

    Adds home fragment

---
+++ app/src/main/java/com/example/fbu_instaclone/Adapter/PostAdapter.java
@@ -0,0 +1,67 @@
+package com.example.fbu_instaclone.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.fbu_instaclone.R;
+import com.example.fbu_instaclone.model.Post;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    Context context;
+    List<Post> posts;
+
+    public PostAdapter(Context context, List<Post> posts){
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivProfile;
+        ImageView ivPicture;
+        TextView tvUserhandle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivPicture = itemView.findViewById(R.id.ivPicture);
+            ivProfile = itemView.findViewById(R.id.ivProfile);
+            tvUserhandle = itemView.findViewById(R.id.tvProfilename);
+        }
+
+        public void bind(Post post){
+            tvUserhandle.setText(post.getUser().getUsername());
+            Glide.with(context)
+                    .load(post.getImage().getUrl())
+                    .into(ivPicture);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fbu_instaclone/Fragments/HomeFragment.java
@@ -0,0 +1,78 @@
+package com.example.fbu_instaclone.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fbu_instaclone.Adapter.PostAdapter;
+import com.example.fbu_instaclone.R;
+import com.example.fbu_instaclone.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    public static final String TAG = "HomeFragment";
+    Context context;
+    List<Post> posts;
+    RecyclerView rvPosts;
+    PostAdapter adapter;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public HomeFragment(Context context){
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        posts = new ArrayList<>();
+        adapter = new PostAdapter(context, posts);
+
+        rvPosts.setLayoutManager(new LinearLayoutManager(context));
+        rvPosts.setAdapter(adapter);
+        queryPost();
+    }
+
+    public void queryPost(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Exception: " + e.getMessage());
+                    return;
+                }
+                else{
+                    posts.addAll(objects);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+}
